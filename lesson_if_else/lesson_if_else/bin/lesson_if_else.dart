074657aea4ab37f52f4e770ccee08514217f37a7
RozaBaitikova <104209160+RozaBaitikova@users.noreply.github.com>
2022-05-16 15:33:50 +0600

    lesson_if_else

---
+++ lesson_if_else/lesson_if_else/bin/lesson_if_else.dart
@@ -0,0 +1,97 @@
+void main(List<String> arguments) {
+int fingerNum = 5;
+if (fingerNum==1){
+  print ('Большой Палец'); 
+} else if (fingerNum==2){
+  print ('Указательный Палец'); 
+}else if (fingerNum==3){
+  print ('Средний Палец');
+  }else if (fingerNum==4){
+  print ('Безымянный Палец');
+  }else if (fingerNum==5){
+  print ('Мизинец');
+  }
+
+ 
+int min = 30; 
+if (min>=00 && min<14) 
+{print ("First half an hour");
+}
+if (min>=15 && min<29)
+{print ('Second half an hour');
+}
+if (min>=30 && min<44)
+{print ('Third half an hour');
+}
+if (min>=45 && min<60) 
+{print ('Fourth half an hour');
+}
+
+
+String lang = 'eng'; 
+if (lang == 'ru')
+{print (['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']);
+}
+if (lang == 'eng')
+{print (['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']); 
+} 
+
+
+String a = 'abcde'; 
+if (a.startsWith('a'))
+{print ('Да');
+}else if (false)
+{print ('Нет'); }
+
+int seasons = 4; 
+if (seasons ==1)
+{print ('Зима');
+} else if (seasons ==2)
+{print ('Весна');
+} else if (seasons == 3)
+{print ('Лето'); 
+} else if (seasons ==4)
+{print('Осень');
+}
+
+int num = 0; 
+if (num<0)
+{print ('Верно'); 
+}else if (num>=0)
+{ (print ('Неверно'));
+}
+
+var chislo = '123321'; 
+if ((chislo[0])+(chislo[1])+(chislo[2])==(chislo[3])+(chislo[4])+(chislo[5])){
+print ('Да'); }
+else {
+  print ('Нет');
+}
+
+
+
+var color = 'жёлтый'; 
+if (color == 'красный') {
+  var action = 'надо стоять'; 
+print (action);
+}  
+ if (color == 'жёлтый'); {
+var action = 'надо приготовиться';
+print (action); 
+} if (color == 'зеленый'){
+var action = 'можно идти'; 
+print (action); }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
